e90374a27ab629075a9f5ce97e4d4e40467183b8
han <pano1215@naver.com>
2021-05-11 11:12:46 +0900

    0511 크로스스레드 문제해결

---
+++ OOPSolution/OOPSolution/BasicThreadTestApp/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading;
+
+namespace BasicThreadTestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //DoSomething();
+            Thread th1 = new Thread(DoSomething);
+            th1.Start();
+            //th1.Join(); //스레드 종료 대기.
+
+            /*th1.Abort(); 더 이상 사용 안함. */
+            th1.Interrupt(); //스레드를 WaitJoinSleep 상태일 때 중단메서드
+            th1.Join();
+            DoOtherthing();
+        }
+        
+        private static void DoOtherthing()
+        {
+            for (int i = 0; i < 30; i++)
+            {
+                Console.WriteLine($"DoOtherthing : {i}");
+                Thread.Sleep(1);
+            }
+        }
+
+        private static void DoSomething()
+        {
+            for (int i = 0; i < 30; i++)
+            {
+                Console.WriteLine($"DoSomething : {i}");
+                Thread.Sleep(1);
+            }
+        }
+    }
+}
---
+++ OOPSolution/OOPSolution/WinformThreadApp/MainForm1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace WinformThreadApp
+{
+    public partial class MainForm1 : Form
+    {
+        public MainForm1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            progressBar1.Minimum = 0;
+            progressBar1.Maximum = 10000;
+
+            
+            
+            // 스레드로 분리 윈폼의 화면스레드, 처리스레드를 분리한 것.
+            Thread th = new Thread(()=> { //람다식
+                for (int i = 0; i <= 10000; i++)
+                {
+                    progressBar1.BeginInvoke(
+                        new Action(() =>
+                        {
+                            progressBar1.Value = i;
+                        })); //크로스 스레드로 문제 해결. 분리된 스레드에 접근하려고하니 오류가 발생함. 이를 해결하기위해 BeginInvoke 사용.
+                    Thread.Sleep(5);
+                }
+            });
+            th.IsBackground = true; // 백그라운드로 실행할지 여부
+            th.Start();
+        }
+    }
+}
